6605e38c338d09d9343d871a92f2b420bf8757f3
bjornarprytz <bjornarprytz@users.noreply.github.com>
2020-06-15 00:01:55 +0200

    Added another method of selection, which should be used when the targets are known without accessing the gamestate.

---
+++ Archetype/Cards/TargetRequirements/SelectionMethods/PreSelectionInfo.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+
+namespace Archetype
+{
+    public class PreSelectionInfo : TargetInfo
+    {
+        public override bool IsAutomatic => true;
+
+        public PreSelectionInfo(params ITarget[] targets)
+        {
+            _minTargets = _maxTargets = targets.Length;
+
+            _options = new List<ITarget>(targets);
+
+            foreach(var target in targets)
+            {
+                Add(target);
+            }
+        }
+    }
+}
